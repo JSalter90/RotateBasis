66cc6fe7add5efcbbfdcfdc4d6a9aa171dd3cdf0
James Salter <jms231@exeter.ac.uk>
2018-08-20 09:18:31 +0100

    Making example consistent with updated code

--- R/example.R
+++ R/example.R
@@ -1,4 +1,4 @@
-source('C:/Users/James/Dropbox/RotateBasis/R/rotation_functions.R')
+source('rotation_functions.R')
 library(GenSA)
 library(lhs)
 library(tensor)
@@ -23,12 +23,15 @@ dim(DataBasis1$tBasis)
 sum(DataBasis1$tBasis[,1] * DataBasis1$tBasis[,2]) # orthogonal basis vectors in L2
 
 tmp <- t(runif(100,0.01,5))
-#W <- t(tmp) %*% tmp + runif(100,0.01,0.1)*diag(100) # some positive definite 100x100 weight matrix
-W <- diag(100)
-diag(W) <- runif(100,0.1,10)
+W <- t(tmp) %*% tmp + runif(100,0.01,0.1)*diag(100) # some positive definite 100x100 weight matrix
 Winv <- GetInverse(W) # invert via chol
 DataBasis2 <- MakeDataBasis(data = output, weightinv = Winv, RemoveMean = TRUE)
 DataBasis2$tBasis[,1] %*% Winv %*% DataBasis2$tBasis[,2] # orthogonal basis vectors in W
+DataBasis2$tBasis[,1] %*% Winv %*% DataBasis2$tBasis[,1] # with length 1
+
+# We have stored the eigendecomposition of Winv in DataBasis2
+# This was needed for wSVD, and will come in useul when calculating the residual basis at each iteration of the rotation
+summary(c(Winv - DataBasis2$Q %*% (diag(DataBasis2$Lambda)) %*% t(DataBasis2$Q)))
 
 # Centre observations by the ensemble mean
 obsc <- obs - DataBasis1$EnsembleMean
@@ -39,31 +42,36 @@ v2 <- VarMSEplot(DataBasis = DataBasis2, obs = obsc, weightinv = Winv)
 
 # Check variances correct
 svd_d <- wsvd(t(DataBasis2$CentredField), weightinv = Winv)$d
-sum(svd_d[1:2]^2 / sum(svd_d^2)) # matches up to that given by VarExplained
+sum(svd_d[1:2]^2 / sum(svd_d^2)) # matches that given by v2
 
 # Project the (centred) ensemble onto the first q vectors of the SVD basis
 q1 <- ExplainT(DataBasis1, vtot = 0.95)
 q2 <- ExplainT(DataBasis2, vtot = 0.95, weightinv = Winv)
 
 Coeffs1 <- CalcScores(data = DataBasis1$CentredField, basis = DataBasis1$tBasis[,1:q1])
-Coeffs2 <- CalcScores(data = DataBasis2$CentredField, basis = DataBasis2$tBasis[,1:q2])
-plot(Coeffs1[,4], Coeffs2[,4], pch = 18)
+Coeffs2 <- CalcScores(data = DataBasis2$CentredField, basis = DataBasis2$tBasis[,1:q2], weightinv = Winv)
 
 # Rotate!
+# Can set MaxTime low here as have small ensemble and l = 100, although MaxTime=30 or 60 usually fine
 RotatedBasis1 <- RotateBasis(DataBasis = DataBasis1, obs = obsc, kmax = 3, v = c(0.4,0.1,0.1), MaxTime = 5)
 RotatedBasis2 <- RotateBasis(DataBasis = DataBasis2, obs = obsc, weightinv = Winv, kmax = 3, v = c(0.4,0.1,0.1), MaxTime = 5)
 # Are resulting vectors orthogonal in W?
-sum(RotatedBasis1$tBasis[,1] * RotatedBasis1$tBasis[,7])
-RotatedBasis2$tBasis[,2] %*% Winv %*% RotatedBasis2$tBasis[,4]
-# Do we minimise R_W?
+sum(RotatedBasis1$tBasis[,1] * RotatedBasis1$tBasis[,4])
+RotatedBasis2$tBasis[,1] %*% Winv %*% RotatedBasis2$tBasis[,2]
+# Do we minimise R_W/do better than before?
 par(mfrow=c(1,2), mar = c(4,2,2,2))
-VarMSEplot(RecVarData = v1, obs = obsc, ylim = c(0,25))
-VarMSEplot(DataBasis = RotatedBasis1, obs = obsc, ylim = c(0,25))
+VarMSEplot(RecVarData = v1, obs = obsc, ylim = c(0,26))
+VarMSEplot(DataBasis = RotatedBasis1, obs = obsc, ylim = c(0,26))
 
-VarMSEplot(RecVarData = v2, obs = obsc, weightinv = Winv, ylim = c(0,5.5))
-VarMSEplot(DataBasis = RotatedBasis2, obs = obsc, weightinv = Winv, ylim = c(0,5.5))
+VarMSEplot(RecVarData = v2, obs = obsc, weightinv = Winv, ylim = c(0,500))
+VarMSEplot(DataBasis = RotatedBasis2, obs = obsc, weightinv = Winv, ylim = c(0,500))
 
+
+
+
+# Other examples to come:
 # Instead define some pattern, find residual basis - check orthogonal in correct norm
+# History matching coefficients efficiently
 
 
 
