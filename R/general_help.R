3de2ef0f191698c80bd47c82fa2390419838fef1
James Salter <jms231@exeter.ac.uk>
2018-08-20 15:21:42 +0100

    writing guidance

--- R/general_help.R
+++ R/general_help.R
@@ -4,6 +4,7 @@
 # observations (vector of length l)
 
 
+
 #### Formatting data ####
 # MakeDataBasis formats the ensemble so that later functions can be applied
 ## $CentredField - the ensemble that the basis will be calculated from. 
@@ -15,6 +16,10 @@
 ##               weighted SVD basis, and will be useful later when calculating the residual
 ##               basis at each iteration of the rotation algorithm
 
+#### Centering observations ####
+# If the basis is calculated from the centred ensemble, also need to subtract the ensemble
+# mean from the observations
+# This is currently a manual operation
 
 
 #### Setting W, W^{-1} ####
@@ -28,5 +33,66 @@
 ## $diagonal - is W^{-1} a diagonal matrix?
 ## $identity - is W^{-1} the identity matrix?
 # These allow computational improvements to be made in other functions
+
+
+#### General functions ####
+# Given data, basis, observations, can
+## project fields onto a basis, giving set of coefficients
+CalcScores(data, basis, weightinv = NULL)
+## reconstruct fields from a set of coefficients
+Recon(coeffs, basis)
+## wrapper that does both operations at once (so can find representation of observations
+## on chosen basis)
+ReconObs(obs, basis, weightinv = NULL)
+## calculating the reconstruction error of the observations (default is to scale by dimension,
+## analogous to MSE)
+ReconError(obs, basis, weightinv = NULL, scale = TRUE)
+## calculating variance explained by a basis/basis vector
+VarExplained(basis, data, weightinv = NULL, total_sum = NULL, psi = NULL, basis_lincom = NULL)
+## the total_sum, psi, basis_lincom options are useful in the rotation algorithm
+
+#### Assessing basis quality ####
+# VarMSEplots show how reconstruction error changes as add basis vectors, compared to
+# the history match bound, so can assess whether, given current ensemble/W, the truncated
+# basis would rule out the observations (horizontal dotted line)
+VarMSEplot(DataBasis, obs, RecVarData = NULL, weightinv=NULL, min.line=TRUE, bound=TRUE, qmax = NULL)
+
+
+#### Basis rotation ####
+# If the truncated SVD basis would rule out the observations, but the remainder of the basis
+# does reduce the reconstruction error further (see the VarMSEplot), then rotation can help
+RotateBasis(DataBasis, obs, kmax = 5, weightinv = NULL, v = c(rep(0.1,5)), vtot = 0.95, prior = NULL,
+                        StoppingRule = TRUE, MaxTime = 60)
+# Key inputs:
+## DataBasis - object containing $CentredField, $tBasis. This is the basis that the rotation
+##             is applied to
+## obs - observation vector, needs to be on same scale/same centering as CentredField
+## kmax - maximum number of iterations. Generally 1 or 2 is sufficient.
+## weightinv - W^{-1}, in form given by GetInverse for efficiency
+## v - a vector giving the minimum proportion of ensemble variability to be explained by
+##     each new basis vector, to ensure that there's enough signal to allow emulation.
+##     Setting this is problem dependent, 10% for each is a good starting point, but there
+##     will be cases where this is not possible (high variability across high number of
+##     dimensions), so an alternative is to use 0.5*proportion explained by each SVD basis
+##     vector. Later versions will include a more automated choice of v.
+## vtot - proportion of ensemble variability that the final truncated basis should explain.
+##        90%, 95%, 99% common choices.
+## prior - generally set as NULL. If n is very large, can speed up optimisation by ignoring
+##         some basis vectors, e.g. just rotate first 100 => prior = 1:100
+## MaxTime - generally quite fast, 60 seconds is fine (for l < 1000, MaxTime < 30 should be ok)
+  
+
+
+
+
+
+  
+  
+
+
+
+# 
+
+
 
 
